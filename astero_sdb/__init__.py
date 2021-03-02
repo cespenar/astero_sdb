f6ff28d12dca26f6f29a42e0b860b2a54b5d4138
cespenar <cespenar1@gmail.com>
2021-03-02 13:19:39 +0100

    added __str__ and __repr__

--- astero_sdb/__init__.py
+++ astero_sdb/__init__.py
@@ -35,6 +35,19 @@ class Star:
 
         Star.num_of_stars += 1
 
+    def __str__(self):
+        return f"{self.name}"
+    
+    def __repr__(self):
+        return (
+            f"Star({self.name}, "
+            f"t_eff={self.t_eff}, t_eff_err_p={self.t_eff_err_p}, t_eff_err_m={self.t_eff_err_m}, "
+            f"log_g={self.log_g}, log_g_err_p={self.log_g_err_p}, log_g_err_m={self.log_g_err_m}, "
+            f"v_rot={self.v_rot}, v_rot_err_p={self.v_rot_err_p}, v_rot_err_m={self.v_rot_err_m}, "
+            f"feh={self.feh}, feh_err_p={self.feh_err_p}, feh_err_m={self.feh_err_m}, "
+            f"frequencies_list={self.frequencies})"
+        )
+
 
 if __name__ == "__main__":
     pass
