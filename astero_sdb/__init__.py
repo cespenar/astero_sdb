71b2aa578cca9d9f61997ee35a03531658732a31
cespenar <cespenar1@gmail.com>
2021-03-02 10:40:06 +0100

    added Star

--- astero_sdb/__init__.py
+++ astero_sdb/__init__.py
@@ -1,3 +1,39 @@
+import numpy as np
+
+
+class Star:
+    """Structure containing observational properties of a star.
+
+    """
+
+    num_of_stars = 0
+
+    def __init__(self, name,
+                 t_eff=None, t_eff_err_p=None, t_eff_err_m=None,
+                 log_g=None, log_g_err_p=None, log_g_err_m=None,
+                 v_rot=None, v_rot_err_p=None, v_rot_err_m=None,
+                 feh=None, feh_err_p=None, feh_err_m=None,
+                 frequencies_list=None):
+        self.name = name
+        self.t_eff = t_eff
+        self.t_eff_err_p = t_eff_err_p
+        self.t_eff_err_m = t_eff_err_m
+        self.log_g = log_g
+        self.log_g_err_p = log_g_err_p
+        self.log_g_err_m = log_g_err_m
+        self.v_rot = v_rot
+        self.v_rot_err_p = v_rot_err_p
+        self.v_rot_err_m = v_rot_err_m
+        self.feh = feh
+        self.feh_err_p = feh_err_p
+        self.feh_err_m = feh_err_m
+        if frequencies_list:
+            self.frequencies = np.genfromtxt(
+                self.name, dtype=None, skip_header=1, names=True)
+        else:
+            self.frequencies = None
+
+        Star.num_of_stars += 1
 
 
 if __name__ == "__main__":
