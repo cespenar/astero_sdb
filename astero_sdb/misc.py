cc2ab1c22d7872a885198917fa03377c6f0b4d68
Jakub Ostrowski <cespenar1@gmail.com>
2022-03-02 13:58:25 +0100

    Added functions to find bottom and top of semiconvective zone.

---
+++ astero_sdb/misc.py
@@ -0,0 +1,79 @@
+import numpy as np
+from mesa_reader import MesaData
+
+
+def find_semiconvection_bottom(profile: MesaData,
+                               verbose: bool = False) -> int:
+    """Finds the zone where the natural semiconvection starts in a MESA model.
+
+    Parameters
+    ----------
+    profile : mesa_reader.MesaData
+        Evolutionary model (MESA profile file) as MesaData object.
+
+    Returns
+    ----------
+    zone_semi_bottom : int
+        The zone where the natural semiconvection starts. If negative, the
+        semiconvective zone was not found.
+
+    """
+
+    zone_semi_bottom = -1
+    delta_nabla = (profile.gradr - profile.gradL)[::-1]
+    for i, delta in enumerate(delta_nabla):
+        if i == 0:
+            if delta <= 0.0:
+                break
+            else:
+                continue
+        if delta_nabla[i - 1] * delta <= 0.0:
+            zone_semi_bottom = profile.zone[::-1][i - 1]
+            break
+    if verbose:
+        print('zone_semi_bottom =', zone_semi_bottom)
+    return zone_semi_bottom
+
+
+def find_semiconvection_top(profile: MesaData,
+                            zone_semi_bottom: int,
+                            verbose: bool = False) -> int:
+    """Finds the zone where the natural semiconvection ends in a MESA model.
+
+    Parameters
+    ----------
+    profile : mesa_reader.MesaData
+        Evolutionary model (MESA profile file) as MesaData object.
+    zone_semi_bottom : int
+        The zone where the natural semiconvection starts.
+
+    Returns
+    ----------
+    zone_semi_top : int
+        The zone where the natural semiconvection ends.
+
+    """
+
+    ind_q08 = np.where(profile.q >= 0.8)[0][-1]
+    zone_semi_top = np.argmax(
+        profile.gradL[ind_q08:zone_semi_bottom - 1]) + ind_q08
+
+    in_conv = False
+    delta_nabla = (profile.gradr - profile.gradL)[
+                  zone_semi_top - 1:zone_semi_bottom - 1]
+    for i, delta in enumerate(delta_nabla):
+        if not in_conv and delta <= 0:
+            continue
+        elif not in_conv and delta > 0:
+            in_conv = True
+            if zone_semi_top - 1 + i >= zone_semi_bottom:
+                print('zone_semi_top >= zone_semi_bottom; this is wrong!')
+                break
+            else:
+                continue
+        elif in_conv and delta <= 0:
+            zone_semi_top = zone_semi_top + i - 1
+            break
+    if verbose:
+        print('zone_semi_top =', zone_semi_top)
+    return zone_semi_top
