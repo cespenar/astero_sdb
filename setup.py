e9b370a9c88d10a8c41399f5c70124a917422125
Jakub Ostrowski <cespenar1@gmail.com>
2022-04-28 10:41:52 +0200

    Updated requirements.txt

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='astero_sdb',
-      version='0.0.55',
+      version='0.0.56',
       description='Tools for asteroseismology of sdB stars using MESA and GYRE models.',
       url='https://github.com/cespenar/astero_sdb',
       author='Jakub Ostrowski',
